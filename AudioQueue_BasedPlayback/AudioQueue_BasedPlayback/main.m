d5f62354b825a6636dbe11d3b06d8866fb3f3ba7
lizhaojie <252487266@qq.com>
2020-04-12 23:51:31 +0800

    [update_lzj]AudioQueueBuffer 待解决读取包数据问题

--- AudioQueue_BasedPlayback/AudioQueue_BasedPlayback/main.m
+++ AudioQueue_BasedPlayback/AudioQueue_BasedPlayback/main.m
@@ -11,22 +11,7 @@
 
 #define kNumberPlaybackBuffers 3
 
-#define kPlaybackFileLocation CFSTR("/Users/xdf_yanqing/Documents/未命名文件夹/1222.caf")
-
-
-#pragma mark user data struct
-    //5.2
-typedef struct MyPlayer {
-    AudioFileID playbackFile;
-    SInt64 packetPosition;
-    UInt32 numPacketsToRead;
-    AudioStreamPacketDescription *packetDescs;
-    Boolean isDone;
-} MyPlayer;
-#pragma mark utility functions
-    //4.2
-    //5.14
-    //5.15
+#define kPlaybackFileLocation CFSTR("/Users/macbookpro/Downloads/12222.aifc")
 static void CheckError(OSStatus error, const char *operation) {
     if (error == noErr) return;
     char errorString[20];
@@ -42,10 +27,93 @@ static void CheckError(OSStatus error, const char *operation) {
     fprintf(stderr, "Error: %s (%s)\n", operation, errorString);
     exit(1);
 }
+
+#pragma mark user data struct
+    //5.2
+typedef struct MyPlayer {
+    AudioFileID playbackFile;
+    SInt64 packetPosition;
+    UInt32 numPacketsToRead;
+    AudioStreamPacketDescription *packetDescs;
+    Boolean isDone;
+} MyPlayer;
+#pragma mark utility functions
+    //4.2
+    //5.14
+static void MyCopyEncoderCookieToQueue(AudioFileID theFile,
+                                       AudioQueueRef queue ) {
+    UInt32 propertySize;
+    OSStatus result = AudioFileGetPropertyInfo (theFile,
+                                                kAudioFilePropertyMagicCookieData, &propertySize,
+                                                NULL);
+    if (result == noErr && propertySize > 0) {
+        Byte* magicCookie = (UInt8*)malloc(sizeof(UInt8) * propertySize);
+        CheckError(AudioFileGetProperty (theFile,
+                                         kAudioFilePropertyMagicCookieData,
+                                         &propertySize, magicCookie),
+                   "Get cookie from file failed");
+        CheckError(AudioQueueSetProperty(queue,
+                                         kAudioQueueProperty_MagicCookie, magicCookie,
+                                         propertySize),
+                   "Set cookie on queue failed");
+        free(magicCookie);
+    }
+}
+    //5.15
+void CalculateBytesForTime (AudioFileID inAudioFile, AudioStreamBasicDescription inDesc,
+                            Float64 inSeconds, UInt32 *outBufferSize, UInt32 *outNumPackets){
+    
+    UInt32 maxPacketSize;
+    UInt32 propSize = sizeof(maxPacketSize);
+    CheckError(AudioFileGetProperty(inAudioFile,kAudioFilePropertyPacketSizeUpperBound, &propSize,
+                                    &maxPacketSize),"Couldn't get file's max packet size");
+    static const int maxBufferSize = 0x10000;
+    static const int minBufferSize = 0x4000;
+    if (inDesc.mFramesPerPacket) {
+        Float64 numPacketsForTime = inDesc.mSampleRate /
+        inDesc.mFramesPerPacket * inSeconds; *outBufferSize = numPacketsForTime * maxPacketSize;
+    } else {
+        *outBufferSize = maxBufferSize > maxPacketSize ?
+        maxBufferSize : maxPacketSize;
+    }
+    if (*outBufferSize > maxBufferSize && *outBufferSize > maxPacketSize)
+        *outBufferSize = maxBufferSize;
+    else {
+        if (*outBufferSize < minBufferSize)
+            *outBufferSize = minBufferSize; *outNumPackets = *outBufferSize / maxPacketSize;
+    }
+}
 #pragma mark playback callback function
     //5.16-5.19
 static void MyAQOutputCallback(void *inUserData, AudioQueueRef inAQ, AudioQueueBufferRef inBuffer){
-    
+    MyPlayer *aqp = (MyPlayer*)inUserData;
+    if (aqp->isDone) return;
+    UInt32 numBytes;
+    UInt32 nPackets = aqp->numPacketsToRead;
+//    CheckError(AudioFileReadPackets(aqp->playbackFile,
+//                                    false,
+//                                    &numBytes,
+//                                    aqp->packetDescs, aqp->packetPosition,
+//                                    &nPackets, inBuffer->mAudioData),
+//               "AudioFileReadPackets failed");
+    CheckError(AudioFileReadPacketData(aqp->playbackFile,
+                                       false,
+                                       &numBytes,
+                                       aqp->packetDescs,
+                                       aqp->packetPosition,
+                                       &nPackets,
+                                       inBuffer->mAudioData),"AudioFileReadPacketData failed");
+
+    if (nPackets > 0) {
+        inBuffer->mAudioDataByteSize = numBytes;
+        AudioQueueEnqueueBuffer(inAQ,
+                                inBuffer, (aqp->packetDescs ? nPackets : 0), aqp->packetDescs);
+        aqp->packetPosition += nPackets;
+    } else {
+        CheckError(AudioQueueStop(inAQ, false),
+                   "AudioQueueStop failed");
+        aqp->isDone = true;
+    }
 }
 
 #pragma mark main function
@@ -53,7 +121,7 @@ static void MyAQOutputCallback(void *inUserData, AudioQueueRef inAQ, AudioQueueB
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-      //open file
+        //open file
         // 5.3-5.4
         MyPlayer player = {0};
         CFURLRef myFileURL = CFURLCreateWithFileSystemPath(kCFAllocatorDefault, kPlaybackFileLocation, kCFURLPOSIXPathStyle, false);
@@ -86,9 +154,9 @@ int main(int argc, const char * argv[]) {
         
         UInt32 bufferByteSize;
         CalculateBytesForTime(player.playbackFile,
-                                                     dataFormat,
-                                                     0.5,
-                                                     &bufferByteSize, &player.numPacketsToRead);
+                              dataFormat,
+                              0.5,
+                              &bufferByteSize, &player.numPacketsToRead);
         
         bool isFormatVBR = (dataFormat.mBytesPerPacket == 0 ||
                             dataFormat.mFramesPerPacket == 0);
@@ -100,11 +168,36 @@ int main(int argc, const char * argv[]) {
         
         MyCopyEncoderCookieToQueue(player.playbackFile, queue);
         
+        AudioQueueBufferRef buffers[kNumberPlaybackBuffers];
+        player.isDone = false;
+        player.packetPosition = 0;
+        int i;
+        for (i = 0; i < kNumberPlaybackBuffers; ++i) {
+            CheckError(AudioQueueAllocateBuffer(queue, bufferByteSize,
+                                                &buffers[i]), "AudioQueueAllocateBuffer failed");
+            MyAQOutputCallback(&player, queue, buffers[i]);
+            if (player.isDone)
+                break;
+        }
         //start queue
         //5.11-5.12
-        
+        CheckError(AudioQueueStart(queue, NULL),
+                   "AudioQueueStart failed");
+        printf("Playing...\n");
+        do
+        {
+            CFRunLoopRunInMode(kCFRunLoopDefaultMode, 0.25,
+                               false);
+        } while (!player.isDone);
+        CFRunLoopRunInMode(kCFRunLoopDefaultMode, 2, false);
         //clean up
         //5.13
+        player.isDone = true;
+        CheckError(AudioQueueStop(queue,
+                                  TRUE), "AudioQueueStop failed");
+        AudioQueueDispose(queue, TRUE);
+        AudioFileClose(player.playbackFile);
+        NSLog(@">>>>>>>>>>");
         
     }
     return 0;
