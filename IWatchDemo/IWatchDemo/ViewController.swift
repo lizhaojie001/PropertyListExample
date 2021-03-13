3a4d472ccb0285d30d5afa0136fb0515e55019bd
lizhaojie <252487266@qq.com>
2021-03-13 15:47:35 +0800

    [update_lzj]iWatchDemo

---
+++ IWatchDemo/IWatchDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IWatchDemo
+//
+//  Created by macbook pro on 2020/12/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
