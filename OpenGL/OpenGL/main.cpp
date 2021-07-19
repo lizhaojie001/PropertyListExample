2a515e40d0297e4d67b85b3ced5caad8b95fb7c6
lizhaojie <252487266@qq.com>
2021-07-19 08:24:16 +0800

    [update_lzj] OpenGL - 画三角形

---
+++ OpenGL/OpenGL/main.cpp
@@ -0,0 +1,160 @@
+//
+//  main.cpp
+//  OpenGL
+//
+//  Created by macbook pro on 2021/7/18.
+//
+
+#include <iostream>
+#include "include/GLFW/glfw3.h"
+#include "include/glad/glad.h"
+#include <OpenGL/OpenGL.h>
+void createWidow();
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+
+//顶点着色器
+const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+const char *fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\n\0";
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+int main(int argc, const char * argv[]) {
+        glfwInit();
+       glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+       glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+       glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+      if (window == NULL)
+      {
+          std::cout << "Failed to create GLFW window" << std::endl;
+          glfwTerminate();
+          return -1;
+      }
+      glfwMakeContextCurrent(window);
+    
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+    
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    
+
+
+    //创建并编译顶点着色器
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    
+    int  success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    
+    if(!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    
+    //创建并编译片段着色器
+ 
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+        glCompileShader(fragmentShader);
+        // check for shader compile errors
+        glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+        if (!success)
+        {
+            glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+            std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+        }
+    //创建着色器程序,链接着色器
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if(!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::glLinkProgram::COMPILATION_FAILED\n" << infoLog << std::endl;
+
+    }
+    //链接之后删除
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+    
+    float vertices[] = {
+            -0.5f, -0.5f, 0.0f, // left
+             0.5f, -0.5f, 0.0f, // right
+             0.0f,  0.5f, 0.0f  // top
+        };
+    
+    unsigned int VBO,VAO;
+    glGenVertexArrays(1,&VAO);
+    glGenBuffers(1, &VBO);
+    glBindVertexArray(VAO);
+  //复制到顶点缓冲对象设置属性
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    //设置读取规则
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    
+    //渲染循环
+    while(!glfwWindowShouldClose(window))
+    {
+        processInput(window);
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shaderProgram);
+
+    
+    
+    glfwTerminate();
+
+    return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+ 
---
+++ OpenGL_GLAD/OpenGL_GLAD/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OpenGL_GLAD
+//
+//  Created by macbook pro on 2021/7/18.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
