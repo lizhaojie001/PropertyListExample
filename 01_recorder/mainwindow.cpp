12407c40f0d63a92c33d5d24d5c50f82d51b07b5
lizhaojie <lizhaojie3@xdf.cn>
2022-04-22 08:45:18 +0800

    [update_lzj] recorder_demo_ffmpeg

---
+++ 01_recorder/mainwindow.cpp
@@ -0,0 +1,105 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QFile>
+#include <QDebug>
+extern "C" {
+
+// 设备相关API
+#include <libavdevice/avdevice.h>
+// 格式相关API
+#include <libavformat/avformat.h>
+// 工具相关API（比如错误处理）
+#include <libavutil/avutil.h>
+// 编码相关API
+#include <libavcodec/avcodec.h>
+
+#include <libavutil/dict.h>
+
+//#include <libavcodec/av
+}
+
+// 格式名称、设备名称目前暂时使用宏定义固定死
+#ifdef Q_OS_WIN
+// 格式名称
+#define FMT_NAME "dshow"
+// 设备名称
+#define DEVICE_NAME "audio=麦克风阵列 (Realtek(R) Audio)"
+#else
+#define FMT_NAME "avfoundation"
+#define DEVICE_NAME ":4"
+#endif
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+    ,m_pNeedClose(false)
+{
+    ui->setupUi(this);
+
+
+    ui->toolButton->setStyleSheet ("border:none");
+
+    // 初始化libavdevice并注册所有输入和输出设备
+    avdevice_register_all();
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_BtnRecorder_clicked()
+{
+
+    //获取输入格式对象
+    AVInputFormat *fmt = av_find_input_format (FMT_NAME);
+    if (!fmt) {
+        qDebug() << "获取格式失败";
+        return;
+    }
+    //打开设备
+    AVFormatContext* ctx = nullptr;
+    const char * device_name = ":2";
+    AVDictionary *options = nullptr;
+    auto ret =  avformat_open_input (&ctx,device_name,fmt,&options);
+    if (ret != 0) {
+        char errorBuffer[1024];
+        av_strerror (ret,errorBuffer,sizeof(errorBuffer));
+        qDebug() << "设备打开失败";
+        qDebug() << errorBuffer;
+        return;
+    }
+
+    //采集数据
+    AVPacket  packet ;
+    av_read_frame(ctx,&packet);
+    qDebug() << packet.size;
+
+    //存储文件
+    const char * filename = "/Users/xdf_yanqing/Downloads/out.pcm";
+    QFile file(filename);
+
+    if( !file.open (QIODevice::WriteOnly)) {
+        //文件不存在就创建,存在就清空文件
+        avformat_close_input(&ctx);
+        qDebug() << "文件创建失败";
+        return;
+    }
+
+    while (av_read_frame(ctx,&packet) == 0 && !m_pNeedClose) {
+        file.write ((char*)packet.data,packet.size);
+    }
+
+    file.close ();
+//    av_packet_free(&(&packet));
+    avformat_close_input(&ctx);
+
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    this->m_pNeedClose = true;
+}
+
---
+++ build-01_recorder-Desktop_Qt_5_15_2_clang_64bit-Debug/ui_mainwindow.h
@@ -0,0 +1,91 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.15.2
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QToolButton>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralwidget;
+    QHBoxLayout *horizontalLayout;
+    QPushButton *BtnRecorder;
+    QToolButton *toolButton;
+    QPushButton *pushButton;
+    QMenuBar *menubar;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
+        MainWindow->resize(800, 600);
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        horizontalLayout = new QHBoxLayout(centralwidget);
+        horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
+        BtnRecorder = new QPushButton(centralwidget);
+        BtnRecorder->setObjectName(QString::fromUtf8("BtnRecorder"));
+        BtnRecorder->setMaximumSize(QSize(100, 16777215));
+
+        horizontalLayout->addWidget(BtnRecorder);
+
+        toolButton = new QToolButton(centralwidget);
+        toolButton->setObjectName(QString::fromUtf8("toolButton"));
+
+        horizontalLayout->addWidget(toolButton);
+
+        pushButton = new QPushButton(centralwidget);
+        pushButton->setObjectName(QString::fromUtf8("pushButton"));
+        pushButton->setMaximumSize(QSize(100, 16777215));
+
+        horizontalLayout->addWidget(pushButton);
+
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 800, 24));
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName(QString::fromUtf8("statusbar"));
+        MainWindow->setStatusBar(statusbar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        BtnRecorder->setText(QCoreApplication::translate("MainWindow", "\345\274\200\345\247\213\345\275\225\351\237\263", nullptr));
+        toolButton->setText(QCoreApplication::translate("MainWindow", "...", nullptr));
+        pushButton->setText(QCoreApplication::translate("MainWindow", "\345\205\263\351\227\255\345\275\225\351\237\263", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
